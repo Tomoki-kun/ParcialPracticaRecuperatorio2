e46fe5d48893823c71073f8cb4f942c64fda5a46
Tomoki-kun <99996920+Tomoki-kun@users.noreply.github.com>
2023-11-13 22:15:33 -0300

    Clase Cliente

---
+++ ParcialPractica2/Cliente.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParcialPractica2
+{
+    internal class Cliente
+    {
+        private string razonSocial;
+        private int codigo;
+        private double saldo;
+        private double tope;
+
+        public Cliente(string nombre, int codigo, double tope)
+        {
+            this.razonSocial = nombre;
+            this.codigo = codigo;
+            this.tope = tope;
+        }
+
+        public void AgregarCompra(double monto)
+        {
+            if (saldo + monto <= tope)
+                saldo += monto;
+        }
+
+        public void AgregarPago(double monto)
+        {
+            saldo -= monto;
+        }
+
+        public double LeerSaldo()
+        {
+            return saldo;
+        }
+    }
+}
