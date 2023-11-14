7df69f1c676e6ef273a0475f18da2d0f294e8100
Tomoki-kun <99996920+Tomoki-kun@users.noreply.github.com>
2023-11-13 22:57:55 -0300

    cochinadas de la lista de clientes

--- ParcialPractica2/ListaClientes.cs
+++ ParcialPractica2/ListaClientes.cs
@@ -26,22 +26,43 @@ namespace ParcialPractica2
             if (listado.BinarySearch(nuevoCliente) < 0)
             {
                 listado.Add(nuevoCliente);
-            }
-            else
                 ret = true;
+            }
             return ret;
         }
         public bool BorrarCliente(int codigo)
         {
-
+            bool ret = false;
+            Cliente cliente = new Cliente("", codigo, 0);
+            listado.Sort();
+            int pos = listado.BinarySearch(cliente);
+            if (pos > -1)
+            {
+                listado.RemoveAt(pos);
+                ret = true;
+            }
+            return ret;
         }
-        public Cliente VerCliente()
+        public Cliente VerCliente(int codigo)
         {
-
+            Cliente cliente = new Cliente("", codigo, 0);
+            Cliente ret = null;
+            listado.Sort();
+            int pos = listado.BinarySearch(cliente);
+            if (pos > -1)
+            {
+                ret = listado[pos];
+            }
+            return ret;
         }
         public Cliente[] ListaCompleta()
         {
-
+            Cliente[] ret = null;
+            for(int i = 0; i < listado.Count; i++)
+            {
+                ret[i] = listado[i];
+            }
+            return ret;
         }
     }
 }
